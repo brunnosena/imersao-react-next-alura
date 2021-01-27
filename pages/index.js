1d8477816681e40be5eba5175e49729c37ea444e
Brunno Sena <brunnow@hotmail.com>
2021-01-27 15:44:02 -0300

    first class

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/brunnosena" />
+      <GitHubCorner projectUrl="https://github.com/brunnosena/imersao-react-next-alura" />
     </QuizBackground>
   );
 }
